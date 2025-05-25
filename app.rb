c6b3dc95a52473cf872909b2e3b553c21f200182
Rhino <mimiri3snsd0420@gmail.com>
2025-05-25 20:36:07 +0900

    initial commit

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'uri'
 
+set :bind, '0.0.0.0'
+set :port, ENV['PORT'] || 4567
+
 SUSHI_LIST = [
   { name: "マグロ", comment: "赤身の王様！", image: "/images/tsuna.jpg" },
   { name: "サーモン", comment: "みんな大好き", image: "/images/salmon.jpg" },
