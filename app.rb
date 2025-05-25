a9b9089eaa1409a5b5d0c61f6640cf8572be3004
Rhino <mimiri3snsd0420@gmail.com>
2025-05-25 17:40:03 +0900

    初回コミット

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'uri'
+
+SUSHI_LIST = [
+  { name: "マグロ", comment: "赤身の王様！", image: "/images/tsuna.jpg" },
+  { name: "サーモン", comment: "みんな大好き", image: "/images/salmon.jpg" },
+  { name: "エビ", comment: "ぷりぷり食感♪", image: "/images/shrimp.jpg" },
+  { name: "いくら", comment: "宝石みたい！", image: "/images/ikura.jpg" },
+  { name: "たまご", comment: "癒し系ネタ", image: "/images/egg.jpg" }
+]
+
+get '/' do
+  erb :index
+end
+
+post '/result' do
+  @sushi = SUSHI_LIST.sample
+  erb :result
+end
